140345b70dc15eb084fb3c8db8a97f394f201841
PythonShinobi <pythonshinobi@gmail.com>
2024-08-30 09:14:22 +0300

    Set minimum size for the main window.

--- src/views/main_window.py
+++ src/views/main_window.py
@@ -4,28 +4,29 @@ from PyQt5.QtWidgets import (
     QVBoxLayout,
     QWidget
 )
+from PyQt5.QtCore import QSize
 
 from .book_list import BookList
-from .book_detail import BookDetail
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle('Book Manager')
-        self.setGeometry(100, 100, 800, 600)
-
         self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
         layout = QVBoxLayout(self.central_widget)
 
         self.book_list_view = BookList()
-        self.book_detail_view = BookDetail()
-
         layout.addWidget(self.book_list_view)
-        layout.addWidget(self.book_detail_view)
 
         self.setup_menu_bar()
+
+        self.setWindowTitle('Book Manager')
+        self.setGeometry(300, 100, 720, 600)
+        
+        # Set the minimum size to default size
+        self.setMinimumSize(QSize(720, 600))
+
+        self.setCentralWidget(self.central_widget)
 
     def setup_menu_bar(self):
         menu_bar = self.menuBar()
@@ -38,4 +39,4 @@ class MainWindow(QMainWindow):
         # Add more actions as needed
 
     def add_book(self):
-        pass
+        self.book_list_view.add_book()
