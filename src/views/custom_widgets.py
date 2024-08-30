e805679f5effb880d44af9473efd17b938a4d21c
PythonShinobi <pythonshinobi@gmail.com>
2024-08-30 08:56:25 +0300

    Create a custom class QFlowLayout to allows the buttons to wrap around automatically when they the edge of the container.

---
+++ src/views/custom_widgets.py
@@ -0,0 +1,80 @@
+from PyQt5.QtCore import Qt, QSize, QPoint, QRect
+from PyQt5.QtWidgets import QLayout, QSizePolicy
+
+class QFlowLayout(QLayout):
+    """This custom layout allows the buttons to wrap around automatically when they 
+    reach the edge of the container. It arranges the widgets in a flow, similar to how 
+    text wraps in a document."""
+    def __init__(self, parent=None, margin=0, spacing=-1):
+        super().__init__(parent)
+        if parent is not None:
+            self.setContentsMargins(margin, margin, margin, margin)
+
+        self.setSpacing(spacing)
+        self.item_list = []
+
+    def addItem(self, item):
+        self.item_list.append(item)
+
+    def count(self):
+        return len(self.item_list)
+
+    def itemAt(self, index):
+        if 0 <= index < len(self.item_list):
+            return self.item_list[index]
+        return None
+
+    def takeAt(self, index):
+        if 0 <= index < len(self.item_list):
+            return self.item_list.pop(index)
+        return None
+
+    def expandingDirections(self):
+        return Qt.Orientations(Qt.Orientation(0))
+
+    def hasHeightForWidth(self):
+        return True
+
+    def heightForWidth(self, width):
+        height = self.doLayout(QRect(0, 0, width, 0), True)
+        return height
+
+    def setGeometry(self, rect):
+        super().setGeometry(rect)
+        self.doLayout(rect, False)
+
+    def sizeHint(self):
+        return self.minimumSize()
+
+    def minimumSize(self):
+        size = QSize(0, 0)
+        for item in self.item_list:
+            size = size.expandedTo(item.minimumSize())
+        size += QSize(2 * self.spacing(), 2 * self.spacing())
+        return size
+
+    def doLayout(self, rect, test_only):
+        x = rect.x()
+        y = rect.y()
+        line_height = 0
+
+        for item in self.item_list:
+            wid = item.widget()
+            space_x = self.spacing() + wid.style().layoutSpacing(
+                QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Horizontal)
+            space_y = self.spacing() + wid.style().layoutSpacing(
+                QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Vertical)
+            next_x = x + item.sizeHint().width() + space_x
+            if next_x - space_x > rect.right() and line_height > 0:
+                x = rect.x()
+                y = y + line_height + space_y
+                next_x = x + item.sizeHint().width() + space_x
+                line_height = 0
+
+            if not test_only:
+                item.setGeometry(QRect(QPoint(x, y), item.sizeHint()))
+
+            x = next_x
+            line_height = max(line_height, item.sizeHint().height())
+
+        return y + line_height - rect.y()
