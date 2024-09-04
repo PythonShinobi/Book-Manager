9c7cdebf3bcd75f5ce1854a27d7320f173dbca8a
PythonShinobi <pythonshinobi@gmail.com>
2024-09-04 14:17:39 +0300

    Set up the about menu bar.

--- src/views/main_window.py
+++ src/views/main_window.py
@@ -8,6 +8,7 @@ from PyQt5.QtWidgets import (
 )
 
 from .book_list import BookList
+from .custom_widgets import AboutDialog
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -40,7 +41,14 @@ class MainWindow(QMainWindow):
         add_book_action.triggered.connect(self.add_book)
         file_menu.addAction(add_book_action)
 
-        # Add more actions as needed
+        about_menu = menu_bar.addMenu('About')
+        about_action = QAction('about', self)
+        about_action.triggered.connect(self.show_about_dialog)
+        about_menu.addAction(about_action)
 
     def add_book(self):
-        self.book_list_view.add_book()
+        self.book_list_view.add_book_dialog()
+
+    def show_about_dialog(self):
+        dialog = AboutDialog()
+        dialog.exec_()  # Show the dialog modally
